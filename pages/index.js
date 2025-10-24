23f157b9a18c503a08fb5f7ec10e68e6cece1594
kiminamikaio <66280859+kiminamikaio@users.noreply.github.com>
2025-10-24 00:03:39 +0000

    pegadinha do malandro

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,10 @@
 function Home() {
-    return <h1>Vc é Incrível</h1>
+    return <h1>
+        Pena de ururbu, 
+        pena de galinha, 
+        se vc ja deu o c#, 
+        da uma risadinha
+        </h1>
 }
 
 export default Home;
